144eeac6b9966d998489cea633dfbf86eba31cc5
ph04 <47356461+ph04@users.noreply.github.com>
2021-11-29 14:08:04 +0100

    Add files via upload

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,6 +1,8 @@
-;;(setq doom-theme 'doom-city-lights)
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula) ;; << This line enables the theme
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
+(setq doom-theme 'doom-dracula)
+(setq display-line-numbers-type 'relative)
+;(setq auto-save-default t)
+(setq confirm-kill-emacs nil)
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 15 :weight 'light))
+(doom/set-frame-opacity 85)
