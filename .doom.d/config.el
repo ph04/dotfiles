88e05ef69a92e2c929f65343104aef4ffba69bf7
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-06 11:31:38 +0100

    Add files via upload

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,3 +6,19 @@
 (setq confirm-kill-emacs nil)
 (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 15 :weight 'light))
 (doom/set-frame-opacity 85)
+
+(defvar +fl/splashcii-query ""
+  "The query to search on asciiur.com")
+
+(defun +fl/splashcii-banner ()
+  (mapc (lambda (line)
+          (insert (propertize (+doom-dashboard--center +doom-dashboard--width line)
+                              'face 'doom-dashboard-banner) " ")
+          (insert "\n"))
+        (split-string (with-output-to-string
+                        (call-process "~/.doom.d/scripts/ascii_splash.sh" nil standard-output nil +fl/splashcii-query))
+                      "\n" t)))
+
+(setq +doom-dashboard-ascii-banner-fn #'+fl/splashcii-banner)
+
+(setq +fl/splashcii-query "")
