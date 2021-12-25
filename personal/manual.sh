2601ea9f3e5ddf440e043a8a681e8387e9afeafd
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-25 14:49:00 +0100

    Update manual.sh

--- personal/manual.sh
+++ personal/manual.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Install Spaceship for Zsh
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+# MANUAL 
+# set ZSH_THEME="spaceship" in .zshrc
+# restart the terminal
+
+# Install Zsh Autosuggestions for Zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# MANUAL
+# add "zsh-autosuggestions" to the plugins (every plugin in a different line)
+# restart the terminal
+paru -Syu
