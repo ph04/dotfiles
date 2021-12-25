0ab636bf5d4ec6b7046fd91d4442d05f6c1ed034
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-25 13:53:04 +0100

    Update install.sh

--- personal/install.sh
+++ personal/install.sh
@@ -4,5 +4,22 @@
 
 # MANUALLY CHECK THAT YOU HAVE INTERNET ACCESS
 
-sudo pacman -S alsa-utils xf86-video-amdgpu xorg-server xorg-xinit firefox neofetch git 
+# Install
+# - base-devel
+# - kernel headers
+# - alsa
+# - gpu drivers
+# - xorg
+# - firefox
+# - neofetch
+# - git
+sudo pacman -S base-devel linux-headers alsa-utils xf86-video-amdgpu xorg-server xorg-xinit firefox neofetch git 
+sudo pamcan -Syu
+
+# Install Rust from rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+export PATH="$PATH"
+
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
