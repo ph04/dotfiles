6c4beeafe88f2caa204b548c03ac236666cdc6f3
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-25 14:20:49 +0100

    Update install.sh

--- personal/install.sh
+++ personal/install.sh
@@ -37,4 +37,10 @@ paru -Syu
 # Install
 # - firefox
 # - neofetch
-paru -S firefox neofetch
+# - neovim
+# - kitty
+# - zsh
+paru -S firefox neofetch neovim kitty zsh
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
