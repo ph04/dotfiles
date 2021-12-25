5f7c124f8c8a375cb5816f787105fc9945b086b2
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-25 14:26:48 +0100

    Update install.sh

--- personal/install.sh
+++ personal/install.sh
@@ -21,26 +21,32 @@ cd $HOME/Scaricati
 mkdir AUR
 cd AUR
 
-# Install paru and rust
+# Install paru and Rust
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 paru
 paru -Syu
 
-# Remove rust from AUR and install rust from rustup
+# Remove Rust from AUR and install Rust from rustup
 paru -R rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 export PATH="$PATH:$HOME/.cargo/bin"
 paru -Syu
 
 # Install
-# - firefox
+# - Firefox
 # - neofetch
 # - neovim
 # - kitty
-# - zsh
+# - Zsh
 paru -S firefox neofetch neovim kitty zsh
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Spaceship for Zsh
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+# MANUAL 
+# set ZSH_THEME="spaceship" in your .zshrc
