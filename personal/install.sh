013d9144e8a8f78b906b20c8125c280d60a51337
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-25 13:50:36 +0100

    Update install.sh

--- personal/install.sh
+++ personal/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# DON'T USE THIS
+# ##################### DON'T USE THIS, IT'S IN WIP #####################
 
 # MANUALLY CHECK THAT YOU HAVE INTERNET ACCESS
 
 sudo pacman -S alsa-utils xf86-video-amdgpu xorg-server xorg-xinit firefox neofetch git 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
