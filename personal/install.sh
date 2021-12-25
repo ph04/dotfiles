7abf56614788aa68202674140003e0b3af1ee231
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-25 14:44:19 +0100

    Update install.sh

--- personal/install.sh
+++ personal/install.sh
@@ -50,4 +50,11 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 # MANUAL 
-# set ZSH_THEME="spaceship" in your .zshrc
+# set ZSH_THEME="spaceship" in .zshrc
+# restart the terminal
+
+# Install Zsh Autosuggestions for Zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# MANUAL
+# add "zsh-autosuggestions" to the plugins (every plugin in a different line)
+# restart the terminal
