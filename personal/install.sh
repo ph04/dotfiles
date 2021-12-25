4be9c3eebcee8951128aad7ceb7d620d3e713d80
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-25 13:53:48 +0100

    Update install.sh

--- personal/install.sh
+++ personal/install.sh
@@ -16,10 +16,11 @@
 sudo pacman -S base-devel linux-headers alsa-utils xf86-video-amdgpu xorg-server xorg-xinit firefox neofetch git 
 sudo pamcan -Syu
 
-# Install Rust from rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-export PATH="$PATH"
-
+# Install paru
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+
+# Install Rust from rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+export PATH="$PATH:$HOME/.cargo/bin"
