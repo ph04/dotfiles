10d2fb1f73195c0ef2f35b85eb6807471bd5039d
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-28 22:45:14 +0100

    Update manual.sh

--- personal/manual.sh
+++ personal/manual.sh
@@ -14,5 +14,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # restart the terminal
 paru -Syu
 
-# Add Kitty in Openbox menu
-# add a terminal entry for Kitty in /etc/xdg/openbox/menu.xml
+# Fix keyboard layout
+localectl set-x11-keymap it
